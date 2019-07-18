8380b7e6724b23eafcc691ea99daea7d117074fa
Troy <troyjmcl85@gmail.com>
2019-07-17 21:52:26 -0600

    Working on videolist and videolistentry to render

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,15 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
+    {props.stuff.map(video => {
+      return <VideoListEntry video={video}/>
+    })}
+    {/* <div><h5><em>{exampleVideoData[0].snippet.title}</em> {exampleVideoData[0].snippet.thumbnails.default.url}</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,21 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+class VideoListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    <div className="video-list-entry media">
+      <div className="media-left media-middle">
+        <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      </div>
+      <div className="media-body">
+        <div className="video-list-entry-title">Video Title</div>
+        <div className="video-list-entry-detail">Video Description</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
