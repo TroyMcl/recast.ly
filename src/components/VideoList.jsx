3e081ef14c16828f47809f842cc1b1ec8e5d481b
Troy <troyjmcl85@gmail.com>
2019-07-18 20:49:54 -0600

    Complete connecting Video List and Player components

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,9 +1,10 @@
 import VideoListEntry from './VideoListEntry.js';
 
-var VideoList = (props) => (
+var VideoList = (props, ) => (
   <div className="video-list">
     {props.videos.map(video => {
-      return <VideoListEntry video={video}/>;
+      console.log(props)
+      return <VideoListEntry video={video} selectVideo={props.selectVideo}/>;
     })}
     {/* <div><h5><em>{exampleVideoData[0].snippet.title}</em> {exampleVideoData[0].snippet.thumbnails.default.url}</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
