c050624b9a7977237da5771002f6e762376c3404
Troy <troyjmcl85@gmail.com>
2019-07-20 14:28:51 -0600

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.get(`https://www.googleapis.com/youtube/v3/search`, {
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    type: 'video',
+    part: 'snippet',
+    videoEmbeddable: true
+  })
+  .done(function (data) {
+    console.log(data);
+    callback(data.items);
+  })
 };
 
 export default searchYouTube;
+
